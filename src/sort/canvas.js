01d55ee2cf03d38a1fe95f77dcebc5f97acb1f41
123 <jillsyeda@gmail.com>
2020-12-31 20:11:14 +0800

    插入排序画图

--- src/sort/canvas.js
+++ src/sort/canvas.js
@@ -2,38 +2,48 @@ const { insertSort } = require('./sort')
 let canvas = document.getElementById('canvas')
 let ctx = canvas.getContext('2d')
 
-let arr = [9, 1, 8, 2, 7, 3, 6, 4, 5]
+function rand(max) {
+  return Math.floor(Math.random() * max);
+}
+
+let o = {}
+let arr = []
+while (arr.length < 20) {
+  let n = rand(20) + 1
+  if (!o[n]) {
+    o[n] = 1
+    arr.push(n)
+  }
+}
 
 let obj = {}
 insertSort(arr, (a, b) => {return a > b}, obj)
 
 // 矩阵数字初始化
 let max = 0
-let min = Number.MAX_VALUE
 for (let number of arr) {
   if (number > max)
     max = number
-  if (number < min)
-    min = number
 }
 
 // 起始位置 画矩形
 let startX = 50
 let startY = 500
 let width = 20
-let padding = 50
+let padding = 35
+let maxH = 200
 
-for (let i = 0; i < obj.data.length; i++) {
-  // 画矩形
-  // 高度基数 200
-  let x = startX + i * padding
-  let y = startY
-  let number = parseInt(obj.data[i] / max * 200)
-  drawMatrix(x, y, width, number)
+function drawArr (ctx, curArr) {
+  for (let i = 0; i < curArr.length; i++) {
+    let x = startX + i * padding
+    let y = startY
+    let number = parseInt(curArr[i] / max * maxH)
+    ctx.strokeStyle = '#000000'
+    drawMatrix(ctx, x, y, width, number)
+  }
 }
-
-function drawMatrix (x, y, w, h) {
-  ctx.strokeStyle = '#000000'
+// 画矩形
+function drawMatrix (ctx, x, y, w, h) {
   ctx.beginPath()
   ctx.moveTo(x, y)
   ctx.lineTo(x + w, y)
@@ -43,61 +53,65 @@ function drawMatrix (x, y, w, h) {
   ctx.stroke()
 }
 
-setTimeout(() => {
-  let d = []
-  let dd = []
-  for (let v of obj.process) {
-    if (v[0] == 0 && d.length > 0) {
-      dd.push([...d])
-      d = []
-    }
-    d.push(v)
+let i = 0
+let frameCnt = 25
+// 动画
+function drawNode (ctx, fx, fy, fh, curArr, x, y) {
+  if (i > frameCnt) {
+    i = 0
+    return
   }
-
-  for (let data of dd) {
-    actionFns[data[0][0]]()
-
-    function draw () {
-
-      window.requestAnimationFrame(draw.bind(this))
-    }
-    break
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  drawArr(ctx, curArr)
+  ctx.strokeStyle = 'red'
+  let x1 = fx - i * (fx - x) / frameCnt
+  drawMatrix(ctx, x1, fy, width, fh)
+  if (x1 < x) {
+    i = 0
+  } else {
+    i++
+    window.requestAnimationFrame(drawNode.bind(this, ctx, fx, fy, fh, curArr, x, y))
   }
+}
 
-}, 1000)
-
-let actionFns = [
-  // p[0]==0的情况
-  (data) => {
-    // 抹去原来的图形
-    let index = data[0][1]
-    let value = data[0][2]
-    let h = parseInt(value / max * 200)
-    let x = startX + index * padding
+function run () {
+  let time = 500
+  let cnt = 0
+  let curArr = [...obj.data]
+  let focus0, focusValue
+  for (let index = 0; index < obj.process.length; index++) {
+    let data = obj.process[index]
+    let idx = data[1]
+    let value = data[2]
+    let x = startX + idx * padding
+    let h = parseInt(value / max * maxH)
     let y = startY - h
-    ctx.clearRect(x - 1, y - 1, width + 2, h + 2)
-  },
-]
+    if (data[0] == 0) {
+      focus0 = data[1]
+      focusValue = data[2]
+    } else {
+      if (data[0] == 2) {
+        curArr[data[1]] = data[2]
+      }
+      setAction(ctx, startX + focus0 * padding, parseInt(focusValue / max * maxH), [...curArr], x, y, time, cnt)
+      if (data[0] == 1) {
+        focus0 = idx
+      }
+      ++cnt
+    }
+  }
+  // 最后去掉焦点图形
+  setTimeout(() => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    drawArr(ctx, curArr)
+  }, time * cnt)
 
-// 开始排序
-// function doAction (p, ms) {
-//   setTimeout(() => {
-//
-//
-//
-//   }, ms)
-// }
-//
-// // 刷新频率 半秒一刷新
-// let i = 500
-// let totalTime = 500 * obj.process.length
-// setInterval(() => {
-//   let n = 0
-//   for (let p of obj.process) {
-//     ++n
-//     doAction(p, i * n)
-//   }
-// }, totalTime)
+  function setAction (ctx, fx, fh, tmp, x, y, time, cnt) {
+    setTimeout(() => {
+      drawNode(ctx, fx, startY, fh, tmp, x, y)
+    }, time * cnt)
+  }
+}
 
-
+run()
 
