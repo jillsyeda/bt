9d7332c1aa73eb3c7aa3cea7f0003f6a964e4e86
123 <jillsyeda@gmail.com>
2020-12-31 23:34:39 +0800

    代码格式化

--- src/sort/sort.js
+++ src/sort/sort.js
@@ -5,26 +5,26 @@
  * @param {{}}detail 比较细节
  */
 module.exports.insertSort = function (arr, fn, detail) {
-  let dArr
-  if (detail) {
-    detail.data = [...arr]
-    detail.process = []
-    dArr = detail.process
-  }
-  for (let i = 1; i < arr.length; i++) {
-    let value = arr[i]
-    dArr && dArr.push([0, i, value])
-    let j = i - 1
-    for (; j >= 0; j--) {
-      dArr && dArr.push([1, j, arr[j]])
-      if (fn(arr[j], value)) {
-        arr[j + 1] = arr[j]
-        dArr && dArr.push([2, j + 1, arr[j]])
-      } else {
-        break
-      }
+    let dArr;
+    if (detail) {
+        detail.data = [...arr];
+        detail.process = [];
+        dArr = detail.process;
     }
-    arr[j + 1] = value
-    dArr && dArr.push([2, j + 1, value])
-  }
-}
+    for (let i = 1; i < arr.length; i++) {
+        let value = arr[i];
+        dArr && dArr.push([0, i, value]);
+        let j = i - 1;
+        for (; j >= 0; j--) {
+            dArr && dArr.push([1, j, arr[j]]);
+            if (fn(arr[j], value)) {
+                arr[j + 1] = arr[j];
+                dArr && dArr.push([2, j + 1, arr[j]])
+            } else {
+                break
+            }
+        }
+        arr[j + 1] = value;
+        dArr && dArr.push([2, j + 1, value]);
+    }
+};
